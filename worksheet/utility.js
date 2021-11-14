69fc8ad06197f6ec934394507e66c8b17293df8b
UP2018781 <91746177+UP2018781@users.noreply.github.com>
2021-11-14 20:42:55 +0000

    idek

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -1,5 +1,14 @@
 function add(a,b) {
     return a+b;
-  }
-  module.exports.add = add;
-  
+}
+module.exports.add = add;
+
+function compare(arr1,arr2){
+  if (arr1 == arr2){
+    console.log(arr1,arr2);
+    return true;
+  } else {
+    return false;
+  };
+}
+module.exports.compare = compare;
